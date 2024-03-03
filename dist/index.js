1ad8d3b299121754adf453f251645cae50411ab7
Amirmohammad <amii_mhmd@yahoo.com>
2024-03-03 14:51:37 +0330

    debugging index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,11 @@
 "use strict";
 let age = 30;
-if (age < 50)
+if (age < 50) {
     age += 10;
+    console.log("Age is less than 50.");
+}
+else {
+    console.log("Age is not less than 50.");
+}
+console.log("Final age value:", age);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+
 let age: number = 30;
-if(age < 50)
-   age += 10; 
+
+if (age < 50) {
+    age += 10;
+    console.log("Age is less than 50.");
+} else {
+    console.log("Age is not less than 50.");
+}
+
+console.log("Final age value:", age);
